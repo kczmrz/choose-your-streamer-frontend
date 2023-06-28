1358cf2c7a55ee0a1220fa0093f35d9a7923c72d
Kczmrz <kczmrz@proton.me>
2023-06-29 01:20:25 +0200

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,6 @@ export default function Home() {
          <FindForm TriggerModal={triggerStreamerModal}/>
          <p className='mt-4 text-3xl font-bold text-white md:text-4xl'>Top streamers:</p>
          <StreamerCard/>   
-         
          <AddStreamerModal isOpening={modalIsOpening} CloseModal={triggerStreamerModal}/> 
       </div>
     </main>
